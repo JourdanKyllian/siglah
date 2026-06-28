253192a47e671218a210641fcc25b95fc9f8c046
KyllianDev <kyllianjourdan@outlook.com>
2026-06-28 20:25:31 +0200

    initial commit

---
+++ src/client/java/fr/siglah/siglah/client/SiglahClient.java
@@ -0,0 +1,10 @@
+package fr.siglah.siglah.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SiglahClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/fr/siglah/siglah/Siglah.java
@@ -0,0 +1,10 @@
+package fr.siglah.siglah;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Siglah implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
